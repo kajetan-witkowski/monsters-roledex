3f7a80dc6884c020ded31aa17433466651c050f3
udemy535 <udemy535@gmail.com>
2020-09-05 20:36:21 +0200

    Updates

---
+++ precache-manifest.a1c9ceae99e4a73597b4e6ecf45bda41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2eea4bb103d5edce7dc833ea936994",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "159f810413383def0582",
+    "url": "/monsters-roledex/static/css/main.899aa3a6.chunk.css"
+  },
+  {
+    "revision": "5c4a86a22d05febd462d",
+    "url": "/monsters-roledex/static/js/2.e98da76a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.e98da76a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159f810413383def0582",
+    "url": "/monsters-roledex/static/js/main.d1a4b5eb.chunk.js"
+  },
+  {
+    "revision": "61663f00e399761c9d63",
+    "url": "/monsters-roledex/static/js/runtime-main.cbaa1562.js"
+  }
+]);
